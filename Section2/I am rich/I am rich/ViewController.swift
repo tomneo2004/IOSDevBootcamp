d0ef151b9372963c3835eea3b7e9c0e484140895
Nelson <tomneo2004@gmail.com>
2019-06-23 18:02:36 +0800

    Section2 in progress

---
+++ Section2/I am rich/I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Nelson on 23/6/19.
+//  Copyright © 2019 Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
