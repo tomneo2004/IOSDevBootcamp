0c7453e48c37ae1c44f5ea210e798da66a4810f6
Nelson <tomneo2004@gmail.com>
2019-06-25 12:30:53 +0800

    Section5

---
+++ Section5/Datatype.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+//var is mutable
+//let is not mutable like constant
+
+var myAge : Int = 32
+
+myAge = 33
+
+let myName : String = "Nelson"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = myName + " punch"
+
+let myFullName2 = "\(myName) Punch and my age \(myAge)"
+
+let wholeNumber : Int = 11
+let text : String = "abc"
+let floatNumber : Float = 2.34
+let doubleNumber : Double = 43.222234234
+let boolean : Bool = true
---
+++ Section5/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Nelson on 25/6/19.
+//  Copyright © 2019 Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        updateDiceImages()
+    }
+
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+}
+
